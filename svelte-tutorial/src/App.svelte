75fa92018a5df12f480d910eba85388c150cc259
ziho <ziho@kakao.com>
2021-06-21 21:46:04 +0900

    Add Todo,Done box

--- svelte-tutorial/src/App.svelte
+++ svelte-tutorial/src/App.svelte
@@ -10,7 +10,8 @@
 		}
 		$todos.push({
 			id,
-			title
+			title,
+			done: false
 		})
 		// 할당을 통해 반응성을 유지하기 위해 svelte는 자기 자신을 할당하는 경우가 많다.
 		$todos = $todos  
@@ -18,18 +19,32 @@
 		id += 1
 	}
 </script>
-
+<div class="board">
 <h1>Svelte Todo List</h1>
 <input bind:value={title} type="text"
 	   on:keydown={(e)=> {e.key === 'Enter' && createTodo() }} /> 
 <button on:click={createTodo}> 
 	Create Todo 
 </button>
-	{#each $todos as todo}
+
+<div class="left">
+	<h2>Todo</h2>
+	{#each $todos.filter(t => !t.done) as todo (todo.id)}
 	<!-- bind로 props도 양방향 데이터 바인딩 가능! -->
-	<!-- <Todo bind:todos={todos} todo={todo} /> -->
-
+	<Todo bind:todos={todos} todo={todo} />
 	<!-- todos 객체를 통체로 넘겨줘야 writable! -->
-	<Todo todos={todos} todo={todo} />
-
+		<!-- <Todo todos={todos} todo={todo} /> -->
 	{/each}
+</div>
+
+<div class="right">
+	<h2>Done</h2>
+	{#each $todos.filter(t => t.done) as todo (todo.id)}
+	<!-- bind로 props도 양방향 데이터 바인딩 가능! -->
+	<Todo bind:todos={todos} todo={todo} />
+	<!-- todos 객체를 통체로 넘겨줘야 writable! -->
+		<!-- <Todo todos={todos} todo={todo} /> -->
+	{/each}
+</div>
+
+</div>
--- svelte-tutorial/src/Todo.svelte
+++ svelte-tutorial/src/Todo.svelte
@@ -1,4 +1,23 @@
 <script>
+    import { quintOut } from 'svelte/easing';
+	import { crossfade } from 'svelte/transition';
+
+	const [send, receive] = crossfade({
+		fallback(node, params) {
+			const style = getComputedStyle(node);
+			const transform = style.transform === 'none' ? '' : style.transform;
+
+			return {
+				duration: 600,
+				easing: quintOut,
+				css: t => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`
+			};
+		}
+	});
+
     export let todo
     export let todos
     let isEdit = false
@@ -17,6 +36,9 @@
     function offEdit(){
         isEdit = false
     }
+    function refreshTodos(){
+        $todos = $todos
+    }
 </script>
 
 {#if isEdit}
@@ -28,8 +50,13 @@
     </div>
 {:else}
     <div>
+        <label in:receive="{{key: todo.id}}"
+                out:send="{{key: todo.id}}"
+        >
+        <input type=checkbox bind:checked={todo.done} on:change={refreshTodos}/>
         {todo.title}
         <button on:click={onEdit}>Edit</button>
         <button on:click={deleteTodo}>Delete</button>
+        </label>
     </div>
 {/if}
